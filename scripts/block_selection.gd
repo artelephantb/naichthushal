43d2a97a4b72517e56c9391343506645bb6aa2bd
ArtElephantB <87944666+artelephantb@users.noreply.github.com>
2025-09-06 10:16:27 -0400

    Added better break stages

--- scripts/block_selection.gd
+++ scripts/block_selection.gd
@@ -6,13 +6,17 @@ extends Node2D
 
 @export var scale_animation: float = 1.2
 
-@onready var break_stages = $'AnimatedSprite2D'
+#@onready var break_stages = $'AnimatedSprite2D'
+@onready var break_stages = $'BreakStages'
 
 var last_pos = Vector2(0, 0)
 var break_timer = 0.0
 
+var speed_scale = 0.0
+
 func _ready() -> void:
-	break_stages.hide()
+	#break_stages.hide()
+	break_stages.scale = Vector2(0.0, 0.0)
 
 func _process(delta: float) -> void:
 	var tile_pos = tilemap.local_to_map(get_global_mouse_position())
@@ -21,24 +25,31 @@ func _process(delta: float) -> void:
 	# Handle block breaking/placing
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		if tilemap.get_cell_tile_data(tile_pos) != null:
+			var tile_break_timer = tilemap.get_cell_tile_data(tile_pos).get_custom_data('BreakTime') * break_modifier
 			if break_timer == -1.0:
-				break_timer = tilemap.get_cell_tile_data(tile_pos).get_custom_data('BreakTime') * break_modifier
-				break_stages.frame = 0
-				break_stages.speed_scale = break_timer / 10
-				break_stages.play('default')
-				break_stages.show()
+				break_timer = tile_break_timer
+				#break_stages.frame = 0
+				#speed_scale = break_timer / 100
+				#break_stages.speed_scale = break_timer / 100
+				#print(break_timer / 100)
+				#break_stages.play('default')
+				#break_stages.show()
 			elif break_timer == 0.0:
 				break_timer = -1.0
 				tilemap.erase_cell(tile_pos)
-				break_stages.hide()
+				break_stages.scale = Vector2(0.0, 0.0)
+				#break_stages.hide()
 			else:
+				#break_stages.frame += speed_scale
+				break_stages.scale += Vector2(1 / tile_break_timer, 1 / tile_break_timer)
 				break_timer -= 1.0
 	elif Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
 		if tilemap.get_cell_tile_data(tile_pos) == null and (position.x + 16 < safe_area.x or position.x > safe_area.z or position.y + 16 < safe_area.y or position.y > safe_area.w):
 			tilemap.set_cell(tile_pos, 0, Vector2(1, 0))
 	elif Input.is_action_just_released('left_click'):
 		break_timer = -1.0
-		break_stages.hide()
+		break_stages.scale = Vector2(0.0, 0.0)
+		#break_stages.hide()
 
 	# Detect mouse movement
 	if last_pos == position:
@@ -46,6 +57,7 @@ func _process(delta: float) -> void:
 			scale.x -= 0.1
 			scale.y -= 0.1
 	else:
+		break_stages.scale = Vector2(0.0, 0.0)
 		last_pos = position
 		break_timer = -1.0
 
